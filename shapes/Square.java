9b5e76b842e6ff0e4761dfe9d2e3d36758cfa24a
christine77777 <christine_gevorgyan@aybschool.am>
2016-11-27 21:19:01 +0400

    homework6

---
+++ shapes/Square.java
@@ -0,0 +1,12 @@
+package shapes;
+
+public class Square {
+	private int length;
+	public Square(int getLength){
+		length = getLength;
+	}
+	public int getArea(){
+		return length * length;
+	}
+
+}
